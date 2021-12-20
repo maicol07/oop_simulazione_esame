d36de0d77442d57005b00cb845706585b7ba6b83
Maicol Battistini <maicolbattistini@live.it>
2021-12-20 11:42:34 +0100

    Add first exercise

---
+++ src/ex2016/a01/t2/e1/VectorBuilderImpl.java
@@ -0,0 +1,58 @@
+package ex2016.a01.t2.e1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class VectorBuilderImpl<X> implements VectorBuilder<X> {
+	private List<X> list = new ArrayList<X>();
+	private boolean buildDone = false;
+	
+	@Override
+	public void addElement(X x) {
+		list.add(x);
+	}
+
+	@Override
+	public void removeElement(int position) {
+		list.remove(position);
+	}
+
+	@Override
+	public void reverse() {
+		Collections.reverse(list);
+	}
+
+	@Override
+	public void clear() {
+		list.clear();
+	}
+
+	@Override
+	public Optional<Vector<X>> build() {
+		Optional<Vector<X>> value = buildDone ? Optional.empty() : Optional.of(new VectorImpl<X>(list));
+		buildDone = true;
+		return value;
+	}
+
+	@Override
+	public Optional<Vector<X>> buildWithFilter(Filter<X> filter) {
+		/*var list = List.copyOf(list);
+		list.
+		Optional<Vector<X>> value = buildDone ? Optional.empty() : Optional.of(new VectorImpl<X>(filteredList));
+		buildDone = true;
+		return value;*/
+		return null;
+	}
+
+	@Override
+	public <Y> VectorBuilder<Y> mapToNewBuilder(Mapper<X, Y> mapper) {
+		var builder = new VectorBuilderImpl<Y>();
+		list.forEach(element -> builder.addElement(mapper.transform(element)));
+		return builder;
+	}
+
+}
---
+++ src/ex2016/a01/t2/e1/VectorImpl.java
@@ -0,0 +1,39 @@
+package ex2016.a01.t2.e1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class VectorImpl<X> implements Vector<X> {
+	private List<X> list = new ArrayList<X>();
+
+	public VectorImpl(List<X> list) {
+		this.list.addAll(list);
+	}
+	
+	@Override
+	public Optional<X> getAtPosition(int position) {
+		try {
+			var element = list.get(position);
+			return Optional.of(element);
+		} catch (IndexOutOfBoundsException exception) {
+			return Optional.empty();
+		}
+	}
+
+	@Override
+	public int size() {
+		return list.size();
+	}
+
+	@Override
+	public List<X> asList() {
+		return List.copyOf(list);
+	}
+
+	@Override
+	public void executeOnAllElements(Executor<X> executor) {
+		list.forEach(element -> executor.execute(element));
+	}
+
+}
